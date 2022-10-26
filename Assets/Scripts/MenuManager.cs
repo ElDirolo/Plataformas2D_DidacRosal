8024721071979297a7230e39c5f37fc7a63d3fc8
ElDirolo <rosaldidac@gmail.com>
2022-10-26 10:09:22 +0200

    haceindocositas

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject title;
+    public GameObject mainButtons;
+    public GameObject optionsMenu;
+
+    public void OpenOptions()
+    {
+        title.SetActive(false);
+        mainButtons.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+}
