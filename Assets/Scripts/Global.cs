c3b051b307ad42c25ae958d65065caa08f539fc3
ElDirolo <rosaldidac@gmail.com>
2022-10-26 09:14:50 +0200

    ConCiudado

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Global : MonoBehaviour
+public static class Global
 {
     public static int nivel;
     public static int tiempoJuego;
