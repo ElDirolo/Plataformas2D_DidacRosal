869dc53092036d97c58a8b6a41e5243e5f084880
ElDirolo <rosaldidac@gmail.com>
2022-11-08 17:18:48 +0100

    soloaniamciones

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,12 +9,23 @@ public class MenuManager : MonoBehaviour
     public GameObject title;
     public GameObject mainButtons;
     public GameObject optionsMenu;
+    public GameObject selectorMenu;
+    public GameObject marcosPersonajes;
 
     public void OpenOptions()
     {
         title.SetActive(false);
         mainButtons.SetActive(false);
         optionsMenu.SetActive(true);
+    }
+
+
+    public void OpenSelector()
+    {
+        title.SetActive(false);
+        mainButtons.SetActive(false);
+        selectorMenu.SetActive(true);
+        marcosPersonajes.SetActive(true);
     }
 
     public void Scene()
@@ -25,5 +36,35 @@ public class MenuManager : MonoBehaviour
     public void LoadNivel()
     {
         Invoke("Scene", 1f);
+    }
+
+
+
+
+
+
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Platforms 1");
+    }
+
+    public void LoadNivel2()
+    {
+        Invoke("Scene2", 1f);
+    }
+
+
+
+
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Platforms 2");
+    }
+
+    public void LoadNivel3()
+    {
+        Invoke("Scene3", 1f);
     }
 }
